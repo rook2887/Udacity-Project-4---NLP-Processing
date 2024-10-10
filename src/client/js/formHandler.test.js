a2e29eac94cca4f5399db0160d5757e1950383af
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-10-10 11:10:24 +0300

    working version

--- src/client/js/formHandler.test.js
+++ src/client/js/formHandler.test.js
@@ -3,13 +3,13 @@
  */
 
 test('use jsdom in this test file', () => {
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
-  });
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
 
 import { handleSubmit } from './formHandler';
 
-// Mock the sendDataToServer function
+// Mocking the sendDataToServer function
 jest.mock('./formHandler', () => ({
   ...jest.requireActual('./formHandler'),
   sendDataToServer: jest.fn(),
@@ -19,11 +19,10 @@ describe('handleSubmit', () => {
   let event;
 
   beforeEach(() => {
-    // Create a mock event object
+    // Create a mock event object and set up a valid value
     event = {
       preventDefault: jest.fn(),
     };
-    // Set up the input field with a valid value
     document.body.innerHTML = `
       <form id="urlForm">
         <input id="name" value="http://example.com" />
@@ -31,6 +30,7 @@ describe('handleSubmit', () => {
     `;
   });
 
+//   tests
   test('should call sendDataToServer with the input value', () => {
     const { sendDataToServer } = require('./formHandler');
 
@@ -41,7 +41,7 @@ describe('handleSubmit', () => {
   });
 
   test('should alert if input is empty', () => {
-    // Set input value to empty
+
     document.getElementById('name').value = '';
     const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
