0e66a2eadd5416621f36b4b462b319c5445b6834
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-10-10 05:00:58 +0300

    before adding API

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,6 @@ import { handleSubmit } from './js/formHandler';
 import { checkForName } from './js/nameChecker';
 
 
-alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,30 +1,46 @@
 // Replace checkForName with a function that checks the URL
-import { checkForName } from './nameChecker'
 
-// If working on Udacity workspace, update this with the Server API URL e.g. `https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api`
-// const serverURL = 'https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api'
-const serverURL = 'https://localhost:8000/api'
-
+const serverURL = 'https://localhost:8000/api';
 const form = document.getElementById('urlForm');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get the URL from the input field
-    const formText = document.getElementById('name').value;
+  // Get the URL from the input field
+  const formText = document.getElementById('name').value;
 
-    // This is an example code that checks the submitted name. You may remove it from your code
-    checkForName(formText);
-    
-    // Check if the URL is valid
- 
-        // If the URL is valid, send it to the server using the serverURL constant above
-      
+  // Check if the URL is valid
+  if (isValidURL(formText)) {
+    // If the URL is valid, send it to the server using the serverURL constant above
+    sendDataToServer(formText);
+  } else {
+    alert('Enter a valid URL');
+  }
+}
+
+// Function to validate the URL
+function isValidURL(string) {
+  const res = string.match(/(http|https):\/\/[^\s/$.?#].[^\s]*/);
+  return res !== null;
 }
 
 // Function to send data to the server
+function sendDataToServer(url) {
+  fetch(serverURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url: url }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Server response:', data);
+      // You can update the UI with the response data if needed
+    })
+    .catch((error) => console.error('Error:', error));
+}
 
 // Export the handleSubmit function
 export { handleSubmit };
-
